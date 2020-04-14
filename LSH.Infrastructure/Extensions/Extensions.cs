be6a75217dc4cf796803b7c583a28e28751dbc57
liushuai <shuai.liu@xiaoyangedu.com>
2020-04-14 12:04:23 +0800

    字符串扩展接口 Signed-off-by: liushuai <shuai.liu@xiaoyangedu.com>

---
+++ LSH.Infrastructure/Extensions/Extensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LSH.Infrastructure.Extensions
+{
+    public static class Extensions
+    {
+
+        /// <summary>
+        /// 指定字符串转Unicode码
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string[] ToUnicode(this string str)
+        {
+            string[] codeArr = new string[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                string coding = "";
+                string curStr = str[i].ToString();
+                for (int j = 0; j < curStr.Length; j++)
+                {
+                    byte[] bytes = Encoding.Unicode.GetBytes(curStr.Substring(j, 1));
+
+                    //取出二进制编码内容  
+                    string lowCode = Convert.ToString(bytes[0], 16);
+
+                    //取出低字节编码内容（两位16进制）  
+                    if (lowCode.Length == 1)
+                    {
+                        lowCode = "0" + lowCode;
+                    }
+
+                    string hightCode = Convert.ToString(bytes[1], 16);
+
+                    //取出高字节编码内容（两位16进制）  
+                    if (hightCode.Length == 1)
+                    {
+                        hightCode = "0" + hightCode;
+                    }
+
+                    coding += (hightCode + lowCode);
+
+                }
+
+                codeArr[i] = coding;
+            }
+            return codeArr;
+        }
+
+        /// <summary>
+        /// 搜索当前指定文本下室友包含某些关键词
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="keywords"></param>
+        /// <returns></returns>
+        public static bool Exist(this string input, string[] keywords)
+        {
+            StringBuilder pattern = new StringBuilder($@"^.*");
+            for (int i = 0; i < keywords.Length; i++)
+            {
+                if (i == keywords.Length - 1)
+                {
+                    pattern.AppendFormat("({0})", keywords[i]);
+                }
+                else
+                {
+                    pattern.AppendFormat("({0})|", keywords[i]);
+                }
+            }
+            pattern.Append(".*$");
+            return Regex.IsMatch(input, pattern.ToString());
+        }
+
+
+    }
+}
---
+++ LSH.UnitTest/Unit_RegexExpression.cs
@@ -0,0 +1,30 @@
+﻿using LSH.Infrastructure.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSH.UnitTest
+{
+
+
+    [TestClass]
+   public class Unit_RegexExpression
+    {
+
+        [TestMethod]
+        public  void GetUnicode()
+        {
+
+
+            //string msg =Encoding.Unicode.get(Encoding.UTF8.GetBytes("中国"));
+
+           // string[] msg = "中国".ToUnicode();
+
+            bool isHas = "A中国saaaa".Exist(new string[] { "中国s","abc"});
+
+        }
+
+
+    }
+}
