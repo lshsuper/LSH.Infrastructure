2931aada68f50adf2023fc674b72f5cb43c1405b
liushuai <shuai.liu@xiaoyangedu.com>
2020-04-16 17:51:50 +0800

    省市数据解析 Signed-off-by: liushuai <shuai.liu@xiaoyangedu.com>

--- LSH.UnitTest/UnitDapperContext.cs
+++ LSH.UnitTest/UnitDapperContext.cs
@@ -28,8 +28,97 @@ namespace LSH.UnitTest
 
             var allAreas = json.ToObject<TData>();
 
+
+            List<DbModel> models = new List<DbModel>() {
+                           new DbModel(){
+                              LevelType=0,
+                               MergerName="中国",
+                                ParentID=0,
+                                 RegionID=100000,
+                                  RegionName="中国",
+                                   ShortName="中国",
+                                    Pinyin="ZhongGuo",
+                                   
+                                    
+                           }
+            };
+
+            //第一层（省）
+            foreach (var pro in allAreas.result[0])
+            {
+
+                string proFullName = pro.fullname;
+                models.Add(new DbModel() {
+                   Latitude=pro.location["lat"],
+                    LevelType=1,
+                     Longitude=pro.location["lng"],
+                      MergerName="中国,"+proFullName,
+                       ParentID= 100000,
+                        Pinyin=string.Join("",pro.pinyin),
+                         ShortName=pro.name,
+                          RegionName=pro.fullname,
+                           RegionID=int.Parse(pro.id),
+                          
+                       
+                });
+
+               
+                //第二层市
+                if (pro.cidx == null || !pro.cidx.Any()) continue;
+
+                foreach (var city in allAreas.result[1].GetRange(pro.cidx[0],pro.cidx[1]-pro.cidx[0]+1))
+                {
+
+                    string cityFullName = city.fullname;
+
+                    models.Add(new DbModel() {
+                        Latitude = city.location["lat"],
+                        LevelType = 2,
+                        Longitude = city.location["lng"],
+                        MergerName = "中国,"+proFullName+","+cityFullName,
+                        ParentID = 100000,
+                        Pinyin = string.Join("", city.pinyin),
+                        ShortName = city.name,
+                        RegionName = city.fullname,
+                        RegionID = int.Parse(city.id),
+                    });
+
+                    //第三层县
+                    
+                    if (city.cidx == null || !city.cidx.Any()) continue;
+
+                    foreach (var area in allAreas.result[2].GetRange(city.cidx[0], city.cidx[1] - city.cidx[0]+1))
+                    {
+
+                        string areaFullName = area.fullname;
+                        models.Add(new DbModel()
+                        {
+                            Latitude = area.location["lat"],
+                            LevelType = 3,
+                            Longitude = area.location["lng"],
+                            MergerName = "中国," + proFullName + "," + cityFullName+","+areaFullName,
+                            ParentID = 100000,
+                            Pinyin = area.pinyin==null?string.Empty:string.Join("", area.pinyin),
+                            ShortName = area.name==null?area.fullname:area.name,
+                            RegionName = area.fullname,
+                            RegionID = int.Parse(area.id),
+                        });
+
+                    }
+
+                }
+
+
+
+            }
+
+
             using (var _ctx = new DapperContext("", DatabaseType.Mysql))
             {
+
+
+                   
+
 
             }
         }
@@ -66,7 +155,7 @@ namespace LSH.UnitTest
         public decimal Longitude { get; set; }
         public decimal Latitude { get; set; }
 
-
+        public  string Pinyin { get; set; }
 
     }
 
